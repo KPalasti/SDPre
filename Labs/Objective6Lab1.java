8e96b662a39235d7d4223cb7900a25fa4ed18388
Kyle Palasti <Kyle.Palasti@gmail.com>
2021-10-03 12:29:44 -0600

    Objective6Lab1 Complete.

---
+++ Labs/Objective6Lab1.java
@@ -0,0 +1,24 @@
+public class Objective6Lab1 {
+  public static void main(String[] args){
+
+    int month = 6;
+
+    switch (month) {
+      case 1:
+        System.out.println("January");
+        break;
+
+      //Insert months from February to May here
+
+      case 6:
+      System.out.println("June");
+      break;
+
+      //Insert months from July to December here
+
+      default:
+        System.out.println("That is not a valid month.");
+        break;
+      }
+  }
+}
