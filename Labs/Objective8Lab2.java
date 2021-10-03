7515be1c539731193e726eab0602f3cb0d8c8230
Kyle Palasti <Kyle.Palasti@gmail.com>
2021-10-03 13:57:00 -0600

    Objective8Lab2 Complete.

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Objective8Lab2 {
+  public static void main(String[] args){
+
+int sumTotal;
+int max;
+max = 21;
+sumTotal = 0;
+
+int counter;
+for(counter=0; counter < max;counter++){
+  sumTotal=sumTotal+counter;
+}
+System.out.println(sumTotal);
+  }
+}
