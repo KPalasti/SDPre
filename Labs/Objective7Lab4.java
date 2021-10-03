3839f119be846c5cb30e8d52d4bdb2118f4b265d
Kyle Palasti <Kyle.Palasti@gmail.com>
2021-10-03 13:20:09 -0600

    Objective7Lab4 Complete.

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4 {
+  public static void main(String[] args){
+
+    int currentSum=0;
+    int counter=1;
+
+    while(counter<=20){
+      currentSum=currentSum+counter;
+      counter = counter+1;
+    }
+    System.out.println(currentSum);
+  }
+}
