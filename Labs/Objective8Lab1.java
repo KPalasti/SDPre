f525b2ee3ed6138b206d0188665e92ae9caca9a0
Kyle Palasti <Kyle.Palasti@gmail.com>
2021-10-03 13:46:30 -0600

    Objective8Lab1 Complete.

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Objective8Lab1{
+  public static void main(String[] args){
+
+    int i;
+    int max = 10;
+
+    int j = 0;
+    for(j=1;j<=max;j++){
+      System.out.println(j);
+    }
+    int k=1;
+    for(int l = max; l > 0; l--){
+      System.out.println(l);
+    }
+
+  }
+}
