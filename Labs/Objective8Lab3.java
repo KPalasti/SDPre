26f193f5661cee85695ea6e196a7ccfc04412b9b
Kyle Palasti <Kyle.Palasti@gmail.com>
2021-10-03 14:04:40 -0600

    Objective8Lab3 Complete.

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,17 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+
+    int max =21;
+    int counter;
+
+    for(counter=1;counter<max;counter++){
+      if(counter%2==0){
+        System.out.println(counter+" is even");
+      }
+      else{
+        System.out.println(counter+" is odd");
+      }
+
+    }
+  }
+}
